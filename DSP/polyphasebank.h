df968afa17ffc1e6448ae1287b53091289976bd8
Digitelektro <viktor@nowtech.hu>
2022-08-07 17:30:58 +0200

    cleanup

--- DSP/polyphasebank.h
+++ DSP/polyphasebank.h
@@ -57,50 +57,6 @@ private:
     std::vector<std::vector<T>> mPases;
 };
 
-/*
-template<typename T>
-struct PolyphaseBank {
-    int phaseCount;
-    int tapsPerPhase;
-    T **phases;
-};
-
-template<class T>
-    inline PolyphaseBank<T> buildPolyphaseBank(int phaseCount, tap<T>& taps) {
-        // Allocate bank
-        PolyphaseBank<T> pb;
-        pb.phaseCount = phaseCount;
-        pb.phases = new T*[phaseCount];
-
-        // Allocate phases
-        pb.tapsPerPhase = (taps.size + phaseCount - 1) / phaseCount;
-        for (int i = 0; i < phaseCount; i++) {
-            pb.phases[i] = new T[pb.tapsPerPhase];
-            std::fill(pb.phases[i], pb.phases[i] +  pb.tapsPerPhase, 0);
-        }
-
-        // Fill phases
-        int totTapCount = phaseCount * pb.tapsPerPhase;
-        for (int i = 0; i < totTapCount; i++) {
-            pb.phases[(phaseCount - 1) - (i % phaseCount)][i / phaseCount] = (i < taps.size) ? taps.taps[i] : 0;
-        }
-
-        // int currentTap = 0;
-        // for (int tap = 0; tap < pb.tapsPerPhase; tap++) {
-        //     for (int phase = 0; phase < phaseCount; phase++) {
-        //         if (currentTap < taps.size) {
-        //             pb.phases[(phaseCount - 1) - phase][tap] = taps.taps[currentTap++];
-        //         }
-        //         else {
-        //             pb.phases[(phaseCount - 1) - phase][tap] = 0;
-        //         }
-        //     }
-        // }
-
-        return pb;
-    }
-*/
-
 } // namespace DSP
 
 #endif // DSP_POLYPHASEBANK_H
