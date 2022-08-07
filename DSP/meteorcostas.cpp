400fb5b1e834d205789a7b73848da67bb1aced46
Digitelektro <djvenczel@msn.com>
2022-08-07 21:05:01 +0200

    variable initialization fix

--- DSP/meteorcostas.cpp
+++ DSP/meteorcostas.cpp
@@ -3,8 +3,9 @@
 namespace DSP {
 
 MeteorCostas::MeteorCostas(float bandWidth, float initPhase, float initFreq, float minFreq, float maxFreq, bool brokenModulation)
-    : PLL(bandWidth, initPhase, initFreq, minFreq, mMaxFreq)
+    : PLL(bandWidth, initPhase, initFreq, minFreq, maxFreq)
     , mBrokenModulation(brokenModulation)
+    , mError(0.0f)
 {
 
 }
